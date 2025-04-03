c27e65e9e50053f5d29b53533ea53dc00ae563d6
David Mac <davidcicchetti@gmail.com>
2025-04-03 19:42:31 +0200

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='GP_CP_models',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     install_requires=[
         'jaxkern @ git+https://github.com/JaxGaussianProcesses/JaxKern.git',
